0348d808bf50baf920cebd58e2868d905402453c
Harshit Pathak <harsht91@gmail.com>
2015-08-31 19:14:35 +0530

    setting target

--- src/android/UserInfo.java
+++ src/android/UserInfo.java
@@ -6,32 +6,43 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.util.Patterns;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Context;
 
 
 public class UserInfo extends CordovaPlugin {
 
+	Context context;
+
+
 	@Override
 	public boolean execute(String action,JSONArray args, CallbackContext callbackContext) throws JSONException
 	{
+		context=this.cordova.getActivity().getApplicationContext();
+
 		if(action.equals("getEmail")){
 			String mEmail = null;
 			Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8+
-			Account[] accounts = AccountManager.get(getBaseContext()).getAccounts();
+			Account[] accounts = AccountManager.get(context).getAccounts();
 			for (Account account : accounts) {
 			    if (emailPattern.matcher(account.name).matches()) {
 				String possibleEmail = account.name;
 				mEmail = possibleEmail;
 			    }
 			}
-			if(mEmail != null)
+			if(mEmail != null){
 				callbackContext.success(mEmail);
-			else
+				return true;
+			}
+			else{
 				callbackContext.error("error fetching email");
+				return false;
+			}
 		}
-	
+		callbackContext.error("Invalid Request");
+		return false;
 	}
 
 }
